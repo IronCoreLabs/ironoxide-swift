1c40d6b1fb189865092d81968dc83faa76e1460d
Ernie Turner <ernie.turner@ironcorelabs.com>
2020-04-16 10:50:06 -0600

    Rename package to better conform to Swift conventions

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "ironoxide",
+    name: "IronOxideSwift",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "ironoxide",
-            targets: ["ironoxide"]),
+            name: "IronOxideSwift",
+            targets: ["IronOxideSwift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ironoxide",
+            name: "IronOxideSwift",
             dependencies: ["libironoxide"]),
         .testTarget(
             name: "IronOxideTests",
-            dependencies: ["ironoxide"]),
+            dependencies: ["IronOxideSwift"]),
     ]
 )
