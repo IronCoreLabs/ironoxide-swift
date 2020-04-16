51096ba4a822ae442ae7c4a5c9c315bef41a7c04
Ernie Turner <ernie.turner@ironcorelabs.com>
2020-04-16 16:44:05 -0600

    Try changing package name to avoid language specifier

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "IronOxideSwift",
+    name: "IronOxide",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "IronOxideSwift",
-            targets: ["IronOxideSwift"]
+            name: "IronOxide",
+            targets: ["IronOxide"]
         ),
     ],
     dependencies: [
@@ -21,12 +21,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "IronOxideSwift",
+            name: "IronOxide",
             dependencies: ["libironoxide"]
         ),
         .testTarget(
             name: "IronOxideTests",
-            dependencies: ["IronOxideSwift"]
+            dependencies: ["IronOxide"]
         ),
     ]
 )
