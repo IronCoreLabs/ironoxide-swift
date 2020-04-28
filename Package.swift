f9be0d7d74a5dd25a20c960fe220860b230d8e8e
Patrick Walsh <patrick.walsh@ironcorelabs.com>
2020-04-28 16:35:23 -0600

    Make paths relative

--- Package.swift
+++ Package.swift
@@ -22,7 +22,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "IronOxide",
-            dependencies: ["libironoxide"]
+            dependencies: ["libironoxide"],
+            swiftSettings: [
+                .unsafeFlags([
+                    "-I../ironoxide-java/cpp/generated/sdk",
+                ]),
+            ],
+            linkerSettings: [
+                .unsafeFlags(["-L../ironoxide-java/target/release"]),
+            ]
         ),
         .testTarget(
             name: "IronOxideTests",
