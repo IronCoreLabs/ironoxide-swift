be6bb22f8cbb9b7dc60c3b1f00b8fa9175d094c1
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2020-05-04 11:46:00 -0600

    removed linux test files

--- Tests/IronOxideTests/IronOxideTests.swift
+++
@@ -1,7 +0,0 @@
-@testable import IronOxide
-import libironoxide
-import XCTest
-
-final class IronOxideTests: XCTestCase {
-    static var allTests: [(String, (IronOxideTests) -> () -> Void)] = []
-}
