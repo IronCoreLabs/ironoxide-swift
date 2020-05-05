fe7502cf0c1ebc2035f5325c5ed146cb96d4b4b1
Ernie Turner <ernie.turner@ironcorelabs.com>
2020-05-05 13:06:30 -0600

    Remove linux-specific unit test files, clean up search tests to use shared device (#31)

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,1 @@
-import XCTest
-
-import IronOxideTests
-
-var tests = [XCTestCaseEntry]()
-tests += IronOxideTests.__allTests()
-
-XCTMain(tests)
+fatalError("Run the tests with `swift test --enable-test-discovery`.")
