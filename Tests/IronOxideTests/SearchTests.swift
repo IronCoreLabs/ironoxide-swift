e58d967b483a5fe6629b13142ecb306e141b82be
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2020-05-13 12:02:56 -0600

    cleanup searchtests a bit

--- Tests/IronOxideTests/SearchTests.swift
+++ Tests/IronOxideTests/SearchTests.swift
@@ -13,6 +13,14 @@ final class SearchTests: ICLIntegrationTest {
         assertCollectionCount(queryTokens, 8)
         assertCollectionCountGreaterThan(dataTokens, 8)
         XCTAssertTrue(queryTokens.isSubset(of: dataTokens))
+    }
+
+    func testTokenizeWithPartition() throws {
+        let searchQuery = "ironcore labs"
+        let groupId = try unwrapResult(primarySdk!.group.create()).groupId
+        let ebis = try unwrapResult(primarySdk!.search.createBlindIndex(groupId: groupId))
+        let bis = try unwrapResult(ebis.initializeSearch(sdk: primarySdk!))
+        let queryTokens = Set(try unwrapResult(bis.tokenizeQuery(query: searchQuery, partitionId: nil)))
         let queryTokens2 = Set(try unwrapResult(bis.tokenizeQuery(query: searchQuery, partitionId: "foo")))
         assertCollectionCount(queryTokens2, 8)
         XCTAssertFalse(queryTokens2.elementsEqual(queryTokens))
