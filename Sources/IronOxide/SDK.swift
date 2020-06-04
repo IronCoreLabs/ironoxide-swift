d21a314768eb61f95902b952b202218684c78328
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2020-06-04 17:03:45 -0600

    add more docs to the namespaces

--- Sources/IronOxide/SDK.swift
+++ Sources/IronOxide/SDK.swift
@@ -7,13 +7,24 @@ import libironoxide
  */
 public class SDK {
     let ironoxide: OpaquePointer
-    /// Document API
+    /**
+     Encrypted documents give your application the ability to keep information secure no matter where it lives.
+     Your users' data is encrypted end-to-end, from the point where the data was initially generated until it gets to your
+     user's device to be used.
+     */
     public let document: DocumentOperations
-    /// Group API
+    /**
+     The `group` namespace provides methods to manage your cryptographic groups. These methods can be used to retrieve existing groups,
+     create new groups, and manage the administrators and members of your group.
+     */
     public let group: GroupOperations
-    /// User API
+    /// The `user` namespace provides methods to act upon the currently authorized user.
     public let user: UserOperations
-    /// Search API
+    /**
+     The `search` namespace provides methods to create and manage encrypted blind search indexes.
+     This provides the ability to index and search over encrypted data without having to decrypt the data first.
+     Indexes are encrypted to a specific group so that every member of the group has the ability to initialize the search index and query against it.
+     */
     public let search: SearchOperations
 
     init(_ instance: OpaquePointer) {
