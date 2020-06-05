0db280e5cbbd30c23f6cdd9479f85b373a20d2d4
Craig Colegrove <34786857+giarc3@users.noreply.github.com>
2020-06-05 13:44:59 -0600

    Add doc comments to structs and functions (#41)

--- Sources/IronOxide/IronOxide.swift
+++ Sources/IronOxide/IronOxide.swift
@@ -2,23 +2,33 @@ import libironoxide
 
 /// Top-level configuration object for IronOxide
 public class IronOxideConfig: SdkObject {
+    /**
+     Constructs a default `IronOxideConfig`.
+
+     The policy cache will have a maximum of 128 entries, and SDK operations will time-out after 30 seconds.
+     */
     public convenience init() {
         self.init(IronOxideConfig_default())
     }
 
     /**
-     * - Parameter policyCaching: policy caching configuration for IronOxide
-     * - Parameter sdkOperationTimeout: timeout for all SDK methods
+     Constructs an `IronOxideConfig` with the provided configuration parameters.
+
+     - parameters:
+        - policyCaching: Policy caching configuration for IronOxide
+        - sdkOperationTimeout: Timeout for all SDK methods
      */
     public convenience init(policyCaching: PolicyCachingConfig, sdkOperationTimeout: Duration?) {
         let timeoutPtr = Util.buildOptionOf(sdkOperationTimeout, CRustClassOptDuration.init)
         self.init(IronOxideConfig_create(policyCaching.inner, timeoutPtr))
     }
 
+    /// Configuration for maximum policy caching size
     public lazy var policyCachingConfig: PolicyCachingConfig = {
         PolicyCachingConfig(IronOxideConfig_getPolicyCachingConfig(inner))
     }()
 
+    /// Time before SDK operations will time-out
     public lazy var sdkOperationTimeout: Duration? = {
         Util.toOption(IronOxideConfig_getSdkOperationTimeout(inner)).map(Duration.init)
     }()
@@ -27,24 +37,32 @@ public class IronOxideConfig: SdkObject {
 }
 
 /**
- * Policy evaluation caching config
- *
- * Since policies are evaluated by the webservice, caching the result can greatly speed
- * up encrypting a document with a PolicyGrant.
+ Policy evaluation caching config
+
+ Since policies are evaluated by the webservice, caching the result can greatly speed
+ up encrypting a document with a PolicyGrant.
  */
 public class PolicyCachingConfig: SdkObject {
+    /**
+     Constructs a default`PolicyCachingConfig`.
+
+     The policy cache will have a maximum of 128 entries.
+     */
     public convenience init() {
         self.init(PolicyCachingConfig_default())
     }
 
     /**
-     * - Parameter maxEntries: maximum number of policy evaluations that will be cached by the SDK.
-     *                         If the maximum number is exceeded, the cache will be cleared prior to storing the next entry.
+     Constructs a `PolicyCachingConfig` with the provided maximum policy cache size.
+
+      - parameter maxEntries: maximum number of policy evaluations that will be cached by the SDK.
+                              If the maximum number is exceeded, the cache will be cleared prior to storing the next entry.
      */
     public convenience init(maxEntries: UInt) {
         self.init(PolicyCachingConfig_create(maxEntries))
     }
 
+    /// Maximum entries in the policy cache
     public lazy var maxEntries: UInt = {
         PolicyCachingConfig_getMaxEntries(inner)
     }()
@@ -53,8 +71,13 @@ public class PolicyCachingConfig: SdkObject {
 }
 
 /**
- * Verify a user given a JWT signed for that user. Returns a Some(UserResult) if the user ID in the JWT exists and is synced with
- * IronCore. If the user doesn't exist, returns a None
+ Verifies the existence of a user using a JWT to identify their user record.
+
+ Returns `nil` if the user could not be found.
+
+ - parameters:
+    - jwt: Valid IronCore or Auth0 JWT
+    - timeout: Timeout for this operation or `nil` for no timeout
  */
 public func userVerify(jwt: String, timeout: Duration? = nil) -> Result<UserResult?, IronOxideError> {
     let timeoutPtr = Util.buildOptionOf(timeout, CRustClassOptDuration.init)
@@ -64,7 +87,13 @@ public func userVerify(jwt: String, timeout: Duration? = nil) -> Result<UserResu
 }
 
 /**
- * Create a new user within the IronCore system.
+ Creates a user.
+
+ - parameters:
+    - jwt: Valid IronCore or Auth0 JWT
+    - password: Password to use for encrypting and escrowing the user's private key
+    - options: User creation parameters
+    - timeout: Timeout for this operation or `nil` for no timeout
  */
 public func userCreate(
     jwt: String,
@@ -82,8 +111,16 @@ public func userCreate(
 }
 
 /**
- * Generates a new device for the user specified in the signed JWT. This will result in a new transform key (from the user's master private key to
- * the new device's public key) being generated and stored with the IronCore Service.
+ Generates a new device for the user specified in the JWT.
+
+ This will result in a new transform key (from the user's master private key to the new device's public key)
+ being generated and stored with the IronCore Service.
+
+ - parameters:
+    - jwt: Valid IronCore or Auth0 JWT
+    - password: Password for the user specified in the JWT
+    - options: Device creation parameters
+    - timeout: Timeout for this operation or `nil` for no timeout
  */
 public func generateNewDevice(
     jwt: String,
@@ -101,21 +138,34 @@ public func generateNewDevice(
 }
 
 /**
- * Initialize IronOxide with a device. Verifies that the provided user/segment exists and the provided device keys are valid and exist for the provided account.
+ Initializes the IronOxide SDK with a device.
+
+ Verifies that the provided user/segment exists and the provided device keys are valid and
+ exist for the provided account.
+
+ - parameters:
+    - device: Device to use to initialize the SDK
+    - config: Configuration options to apply to the resulting SDK
  */
 public func initialize(device: DeviceContext, config: IronOxideConfig = IronOxideConfig()) -> Result<SDK, IronOxideError> {
     Util.toResult(IronOxide_initialize(device.inner, config.inner)).map(SDK.init)
 }
 
 /**
- * Initialize IronOxide with a device, then rotate private keys where necessary for the user and their groups.
- *
- * Verifies that the provided user/segment exists and the provided device keys are valid and exist for the provided account.
- * After initialization, checks whether the calling user's private key needs rotation and rotates it if necessary,
- * then does the same for each group the user is an admin of.
- *
- * This takes an optional `timeout` that is used for the rotation call. This is separate from the `config` timeout because it is expected
- * that rotation could take significantly longer than other operations.
+ Initializes IronOxide with a device, then rotates private keys where necessary for the user and their groups.
+
+ Verifies that the provided user/segment exists and the provided device keys are valid and exist for the provided account.
+ After initialization, checks whether the calling user's private key needs rotation and rotates it if necessary,
+ then does the same for each group the user is an admin of.
+
+ This takes an optional `timeout` that is used for the rotation call. This is separate from the `config` timeout because it is expected
+ that rotation could take significantly longer than other operations.
+
+ - parameters:
+    - device: Device to use to initialize the SDK
+    - password: User's password needed for rotation
+    - config: Configuration options to apply to the resulting SDK
+    - timeout: Timeout to use for rotation
  */
 public func initializeAndRotate(
     device: DeviceContext,
--- Sources/IronOxide/SDK.swift
+++ Sources/IronOxide/SDK.swift
@@ -1,13 +1,30 @@
 import libironoxide
 
 /**
- * Struct that represents an initialized IronOxide SDK. Contains properties and methods for all SDK functionality
+ Struct that represents an initialized IronOxide SDK.
+
+ Contains properties and methods for all SDK functionality
  */
 public class SDK {
     let ironoxide: OpaquePointer
+    /**
+     Encrypted documents give your application the ability to keep information secure no matter where it lives.
+     Your users' data is encrypted end-to-end, from the point where the data was initially generated until it gets to your
+     user's device to be used.
+     */
     public let document: DocumentOperations
+    /**
+     The `group` namespace provides methods to manage your cryptographic groups. These methods can be used to retrieve existing groups,
+     create new groups, and manage the administrators and members of your group.
+     */
     public let group: GroupOperations
+    /// The `user` namespace provides methods to act upon the currently authorized user.
     public let user: UserOperations
+    /**
+     The `search` namespace provides methods to create and manage encrypted blind search indexes.
+     This provides the ability to index and search over encrypted data without having to decrypt the data first.
+     Indexes are encrypted to a specific group so that every member of the group has the ability to initialize the search index and query against it.
+     */
     public let search: SearchOperations
 
     init(_ instance: OpaquePointer) {
@@ -18,6 +35,11 @@ public class SDK {
         search = SearchOperations(ironoxide)
     }
 
+    /**
+     Clears all entries from the policy cache.
+
+     Returns the number of entries cleared from the cache.
+     */
     public func clearPolicyCache() -> UInt {
         IronOxide_clearPolicyCache(ironoxide)
     }
